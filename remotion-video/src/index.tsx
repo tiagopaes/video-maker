ce8fb4412205475915752ce8633282adc1883b02
tiagopaes <tiagopaes37@gmail.com>
2021-05-22 14:41:38 -0300

    upgrade remotion

--- remotion-video/src/index.tsx
+++ remotion-video/src/index.tsx
@@ -1,19 +1,21 @@
-import {registerRoot, Composition} from 'remotion';
-import {Video} from './Video';
+import { registerRoot, Composition } from 'remotion';
+import { Video } from './Video';
+import players from './assets/players-data.json';
 
 const RemotionVideo: React.FC = () => {
-	return (
-		<>
-			<Composition
-				id="Video"
-				component={Video}
-				durationInFrames={9300}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-		</>
-	);
+  return (
+    <>
+      <Composition
+        id="Video"
+        component={Video}
+        durationInFrames={9300}
+        fps={30}
+        width={1920}
+        height={1080}
+        defaultProps={{ players }}
+      />
+    </>
+  );
 };
 
 registerRoot(RemotionVideo);
