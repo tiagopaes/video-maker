6fac3bb257eab00c705ec1e16d69208a50a79d76
tiagopaes <tiagopaes37@gmail.com>
2021-06-01 22:10:21 -0300

    fix video bot

--- robots/video.js
+++ robots/video.js
@@ -1,11 +1,6 @@
-const fs = require('fs');
 const { execSync } = require('child_process');
 
 function robot(players) {
-  const contentFilePath =
-    __dirname + '/../remotion-video/.data/players-data.json';
-  fs.writeFileSync(contentFilePath, JSON.stringify(players));
-
   const remotionPath = __dirname + '/../remotion-video';
 
   execSync(`npm run build -- --props='${JSON.stringify({ players })}'`, {
